c8cc785e834f3e6d8df5d1654a3a7bc368c2725b
たま吉さん(Tamakichi-San) <tamacat2014@gmail.com>
2018-07-14 14:40:17 +0900

    STM32用サンプルプログラムの追加、ドキュメント修正

--- README.md
+++ README.md
@@ -1,35 +1,49 @@
 # Arduino-W25Q64
-Arduino用 SPI接続フラッシュメモリW25Q64ライブラリ
+Arduino用 SPI接続フラッシュメモリW25Q64ライブラリ  
 
-![結線図](img/demo.jpg)
+![結線図](img/demo.jpg)  
 
-##概要
+## 概要
 Arduinoでwinbond社製 SPI接続フラッシュメモリ**W25Q64**を利用するためのライブラリです。  
 **W25Q32**でも利用出来ます。  
 
-![W25Q64 DIP](img/W25Q64.jpg)
+![W25Q64 DIP](img/W25Q64.jpg)  
 
 
 ライブラリ名称: **W25Q64**  
 ヘッダファイル: `#include <W25Q64.h>`  
 
-##インストール
+## インストール
 **W25Q64**フォルダを**Arduino IDE**の**libraries**フォルダに置いて下さい。  
 
-##回路図
+## 回路図
 **W25Q64**は3.3V稼働のため、5V稼働のArduinoで利用する場合はレベルシフト等の対応を行って下さい。  
 (電源部のパスコンは省略しても動くようです)  
 
-![回路図](img/Schematic1.png)
-![接続図](img/Schematic2.png)
+![回路図](img/Schematic1.png)  
+![接続図](img/Schematic2.png)  
 
 3.3V稼働可能なArduinoは直接接続で利用します。  
 ![接続図2](img/Schematic3.jpg)  
 
-##サンプルプログラム
-スケッチ名：**sample.ino**  
-**W25Q64**に対して、書き込みと読み込みを行いその結果を表示します。  
+
+
+Arduino STM32 Blue Pillボードでの利用例  
+利用するポートは、関数`void W25Q64_seSPIPort(SPIClass& rSPI);`で指定出来ます。  
+![接続図2](img/Schematic4.png)   
+
+
+
+## サンプルプログラム
+
+スケッチ名：**sample.ino**  (Arduino Uno用)  
+**W25Q64**に対して、書き込みと読み込みを行いその結果を表示します。    
 
 シリアル通信の速度は115200で接続します。  
-![接続図](img/sample_console.png)
+![接続図](img/sample_console.png)  
+
+スケッチ名：**sample_STM32.ino**  (Arduino STM32 BluePill用)  
+sample.inoと同じ機能です。  
+スケッチ内の**E25Q64_SPI_PORT**の設定にて利用するSPIポートの変更が可能です。  
+
 
---
+++ W25Q64/examples/sample_STM32/W25Q64_sample.ino
@@ -0,0 +1,158 @@
+//
+// SPI シリアルフラッシュメモリ W25Q64 操作検証プログラム
+// W25Q64のメモリ領域構造
+//   総バイト数 8388608
+//   メモリ空間 24ビットアドレス指定 0x00000 - 0x7FFFFF 
+//   ブロック数 128 (64KB/ブロック)
+//   セクタ数 2048  ( 4KB/セクタ)
+//   総セクタ数 2048
+
+#include <string.h>
+#include <W25Q64.h>
+
+// 利用するSPIポートの指定
+#define E25Q64_SPI_PORT 2 // 1:SPI1, 2:SPI2, 3:SPI3
+
+// CSピンの指定
+#define E25Q64_CS  PB0
+
+// SPI1でない場合、SPIオブジェクトを用意する
+#if E25Q64_SPI_PORT != 1
+  SPIClass  MySPI(E25Q64_SPI_PORT);
+#endif
+
+//
+// 書込みデータのダンプリスト
+// dt(in) : データ格納先頭アドレス
+// n(in)  : 表示データ数
+//
+void dump(byte *dt, uint32_t n) {
+  uint32_t sz;
+  char buf[64];
+  uint16_t clm = 0;
+  byte data;
+  byte sum;
+  byte vsum[16];
+  byte total =0;
+  uint32_t saddr =0;
+  uint32_t eaddr =n-1;
+  sz = eaddr -saddr;
+  
+  Serial.println("----------------------------------------------------------");
+  for (uint16_t i=0;i<16;i++) vsum[i]=0;  
+  for (uint32_t addr = saddr; addr <= eaddr; addr++) {
+    data = dt[addr];
+    if (clm == 0) {
+      sum =0;
+      sprintf(buf,"%05lx: ",addr);
+      Serial.print(buf);
+    }
+
+    sum+=data;
+    vsum[addr % 16]+=data;
+    
+    sprintf(buf,"%02x ",data);
+    Serial.print(buf);
+    clm++;
+    if (clm == 16) {
+      sprintf(buf,"|%02x ",sum);
+      Serial.print(buf);      
+      Serial.println("");
+      clm = 0;
+    }    
+  }
+  Serial.println("----------------------------------------------------------");
+  Serial.print("       ");
+  for (uint16_t i=0; i<16;i++) {
+    total+=vsum[i];
+    sprintf(buf,"%02x ",vsum[i]);
+    Serial.print(buf);
+  }
+  sprintf(buf,"|%02x ",total);
+  Serial.print(buf);      
+  Serial.println("");
+  Serial.println("");
+}
+
+void setup() {
+    byte buf[256];        // 取得データ
+    byte wdata[16];       // 書込みデータ
+    uint16_t n;           // 取得データ数
+    Serial.begin(115200);
+    delay(1000);
+
+#if E25Q64_SPI_PORT !=1
+    W25Q64_seSPIPort(MySPI);
+    W25Q64_begin(E25Q64_CS,18000000);  // フラッシュメモリ利用開始
+#else
+    W25Q64_begin(E25Q64_CS,32000000);  // フラッシュメモリ利用開始
+#endif
+    // JEDEC IDの取得テスト
+    W25Q64_readManufacturer(buf);
+    Serial.print("JEDEC ID : ");
+    for (byte i=0; i< 3; i++) {
+      Serial.print(buf[i],HEX);
+      Serial.print(" ");
+    }
+    Serial.println("");
+    
+    // Unique IDの取得テスト
+    W25Q64_readUniqieID(buf);
+    Serial.print("Unique ID : ");
+    for (byte i=0; i< 7; i++) {
+      Serial.print(buf[i],HEX);
+      Serial.print(" ");
+    }
+    Serial.println("");
+    
+    // データの読み込み(アドレス0から256バイト取得)
+    memset(buf,0,256);
+    n =  W25Q64_read(0,buf, 256);
+    Serial.print("Read Data: n=");
+    Serial.println(n,DEC);
+    dump(buf,256);
+
+    // 高速データの読み込み(アドレス0から256バイト取得)
+    memset(buf,0,256);
+    n =  W25Q64_fastread(0,buf, 256);
+    Serial.print("Fast Read Data: n=");
+    Serial.println(n,DEC);
+    dump(buf,256);
+
+    // セクタ単位の削除
+    n = W25Q64_eraseSector(0,true);
+    Serial.print("Erase Sector(0): n=");
+    Serial.println(n,DEC);
+    memset(buf,0,256);
+    n =  W25Q64_read (0,buf, 256);
+    dump(buf,256);
+ 
+    // データ書き込みテスト
+    for (byte i=0; i < 16;i++) {
+      wdata[i]='A'+i;
+    }  
+    n =  W25Q64_pageWrite(0, 10, wdata, 16);
+    Serial.print("page_write(0,10,d,16): n=");
+    Serial.println(n,DEC);
+    memset(buf,0,256);
+    n =  W25Q64_read (0,buf, 256);
+    dump(buf,256);
+
+    // ステータスレジスタ1の取得
+    buf[0] = W25Q64_readStatusReg1();
+    Serial.print("Status Register-1: ");
+    Serial.print(buf[0],BIN);
+    Serial.println("");
+
+    // ステータスレジスタ2の取得
+    buf[0] = W25Q64_readStatusReg2();
+    Serial.print("Status Register-2: ");
+    Serial.print(buf[0],BIN);
+    Serial.println("");
+}
+
+void loop() {
+
+}
+
+
